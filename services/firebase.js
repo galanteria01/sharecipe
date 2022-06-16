7d6d1eb224a7dc4ed9e71a8404ee6b644e793b14
Shoury Sharma <shanuu12e@gmail.com>
2022-06-16 15:21:40 +0530

    fix: Remove important variables

--- services/firebase.js
+++ services/firebase.js
@@ -1,11 +1,21 @@
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID
+} from '@env'
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBlj2LXGJaA7wngME3Pnn-yktl1Hd8SW5c",
-  authDomain: "sharecipe-aa469.firebaseapp.com",
-  projectId: "sharecipe-aa469",
-  storageBucket: "sharecipe-aa469.appspot.com",
-  messagingSenderId: "566898633668",
-  appId: "1:566898633668:web:da4b7067bb7bf207005291",
-  measurementId: "G-X4Z5P9SFSZ"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID
 };
 
 export { firebaseConfig };
