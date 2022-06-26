a714cbfb1e85b7da861fd5e913228de2ab3a1774
Shoury Sharma <shanuu12e@gmail.com>
2022-06-26 17:45:26 +0530

    enhance: Add brand images

--- screens/app/Tabs.jsx
+++ screens/app/Tabs.jsx
@@ -15,9 +15,24 @@ const Tabs = () => {
         tabBarActiveTintColor: COLORS.green,
       }}
     >
-      <Tab.Screen name="Home" component={Home} options={{ tabBarIcon: ({ color, size }) => (<Icon type="ionicon" name="home" color={color} size={size} />) }} />
-      <Tab.Screen name="Search" component={Search} options={{ tabBarIcon: ({ color, size }) => (<Icon type="ionicon" name="search" color={color} size={size} />) }} />
-      <Tab.Screen name="Profile" component={Profile} options={{ tabBarIcon: ({ color, size }) => (<Icon type="ionicon" name="person" color={color} size={size} />) }} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (<Icon type="ionicon" name="home" color={color} size={size} />)
+        }} />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (<Icon type="ionicon" name="search" color={color} size={size} />)
+        }} />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (<Icon type="ionicon" name="person" color={color} size={size} />)
+        }} />
     </Tab.Navigator>
   )
 }
--- services/firebase.js
+++ services/firebase.js
@@ -9,13 +9,13 @@ import {
 } from '@env'
 
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID,
-  measurementId: MEASUREMENT_ID
+  apiKey: "AIzaSyBlj2LXGJaA7wngME3Pnn-yktl1Hd8SW5c",
+  authDomain: "sharecipe-aa469.firebaseapp.com",
+  projectId: "sharecipe-aa469",
+  storageBucket: "sharecipe-aa469.appspot.com",
+  messagingSenderId: "566898633668",
+  appId: "1:566898633668:web:da4b7067bb7bf207005291",
+  measurementId: "G-X4Z5P9SFSZ"
 };
 
 export { firebaseConfig };
